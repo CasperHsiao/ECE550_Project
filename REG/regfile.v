6cfc36c6c6cf2bcaaf88ee42cebafd135d803df0
Hussain Alkhateeb <hsoon97@gmail.com>
2021-10-03 17:13:03 -0400

    done with read port

--- REG/regfile.v
+++ REG/regfile.v
@@ -12,14 +12,12 @@ module regfile (
 
    output [31:0] data_readRegA, data_readRegB;
 	
-	wire [31:0] write_oneHot, write_enable;
-	wire [31:0] q_reg0, q_reg1, q_reg2, q_reg3, q_reg4, q_reg5, q_reg6, q_reg7, q_reg8, q_reg9, q_reg10, q_reg11, q_reg12,
-	 q_reg13, q_reg14, q_reg15, q_reg16, q_reg17, q_reg18, q_reg19, q_reg20, q_reg21, q_reg22, q_reg23, q_reg24, q_reg25,
-	  q_reg27, q_reg28, q_reg29, q_reg30, q_reg31;
+	wire [31:0] write_oneHot, write_enable, readA_oneHot, readB_oneHot;
+	wire [31:0] q_reg0 [31:0];
+	tri [31:0] data_readRegA, data_readRegB;
 
-   /* YOUR CODE HERE */
 	// Write port
-	decoder(ctrl_writeReg, write_oneHot);
+	decoder writeDecoder(ctrl_writeReg, write_oneHot);
 	genvar i;
 	generate
 		for (i = 0; i < 32; i = i+1)
@@ -28,38 +26,35 @@ module regfile (
 		end
 	endgenerate
 	
-	register reg0(q_reg0, data_writeReg, clk, write_enable[0], 1'b1);
-	register reg1(q_reg1, data_writeReg, clk, write_enable[1], ctrl_reset);
-	register reg2(q_reg2, data_writeReg, clk, write_enable[2], ctrl_reset);
-	register reg3(q_reg3, data_writeReg, clk, write_enable[3], ctrl_reset);
-	register reg4(q_reg4, data_writeReg, clk, write_enable[4], ctrl_reset);
-	register reg5(q_reg5, data_writeReg, clk, write_enable[5], ctrl_reset);
-	register reg6(q_reg6, data_writeReg, clk, write_enable[6], ctrl_reset);
-	register reg7(q_reg7, data_writeReg, clk, write_enable[7], ctrl_reset);
-	register reg8(q_reg8, data_writeReg, clk, write_enable[8], ctrl_reset);
-	register reg9(q_reg9, data_writeReg, clk, write_enable[9], ctrl_reset);
-	register reg10(q_reg10, data_writeReg, clk, write_enable[10], ctrl_reset);
-	register reg11(q_reg11, data_writeReg, clk, write_enable[11], ctrl_reset);
-	register reg12(q_reg12, data_writeReg, clk, write_enable[12], ctrl_reset);
-	register reg13(q_reg13, data_writeReg, clk, write_enable[13], ctrl_reset);
-	register reg14(q_reg14, data_writeReg, clk, write_enable[14], ctrl_reset);
-	register reg15(q_reg15, data_writeReg, clk, write_enable[15], ctrl_reset);
-	register reg16(q_reg16, data_writeReg, clk, write_enable[16], ctrl_reset);
-	register reg17(q_reg17, data_writeReg, clk, write_enable[17], ctrl_reset);
-	register reg18(q_reg18, data_writeReg, clk, write_enable[18], ctrl_reset);
-	register reg19(q_reg19, data_writeReg, clk, write_enable[19], ctrl_reset);
-	register reg20(q_reg20, data_writeReg, clk, write_enable[20], ctrl_reset);
-	register reg21(q_reg21, data_writeReg, clk, write_enable[21], ctrl_reset);
-	register reg22(q_reg22, data_writeReg, clk, write_enable[22], ctrl_reset);
-	register reg23(q_reg23, data_writeReg, clk, write_enable[23], ctrl_reset);
-	register reg24(q_reg24, data_writeReg, clk, write_enable[24], ctrl_reset);
-	register reg25(q_reg25, data_writeReg, clk, write_enable[25], ctrl_reset);
-	register reg26(q_reg26, data_writeReg, clk, write_enable[26], ctrl_reset);
-	register reg27(q_reg27, data_writeReg, clk, write_enable[27], ctrl_reset);
-	register reg28(q_reg28, data_writeReg, clk, write_enable[28], ctrl_reset);
-	register reg29(q_reg29, data_writeReg, clk, write_enable[29], ctrl_reset);
-	register reg30(q_reg30, data_writeReg, clk, write_enable[30], ctrl_reset);
-	register reg31(q_reg31, data_writeReg, clk, write_enable[31], ctrl_reset);
+	generate
+		for (i = 1; i < 32; i = i+1)
+		begin : write_loop
+			register reg1(q_reg0[i], data_writeReg, clock, write_enable[i], ctrl_reset);
+		end
+	endgenerate
+	
+	register reg0(q_reg0[0], data_writeReg, clock, write_enable[0], 1'b1);
+	
+	// Read Port
+	decoder readDecoderA(ctrl_readRegA,readA_oneHot);
+	
+	generate
+		for (i = 0; i < 32; i = i+1)
+		begin : tristate_buffer_loop
+			triBuff readA(.out(data_readRegA), .in(q_reg0[i]), .ctrl(readA_oneHot[i]));
+		end
+	endgenerate
+	
+	
+	
+	decoder readDecoderB(ctrl_readRegB,readB_oneHot);
+
+	generate
+		for (i = 0; i < 32; i = i+1)
+		begin : tristate_buffer_loop_B
+			triBuff readB(.out(data_readRegB), .in(q_reg0[i]), .ctrl(readB_oneHot[i]));
+		end
+	endgenerate
 	
 
 endmodule
--- REG/register.v
+++ REG/register.v
@@ -3,46 +3,46 @@ module register(q, d, clk, en, clr);
 	input clk, en, clr;
 	output [31:0] q;
 	
-//	genvar i;
-//		generate
-//			for (i = 0; i < 31; i = i+1)
-//			begin : dff_loop
-//				dffe_ref dff0(q[i], d[i], clk, en, clr);
-//			end
-//	endgenerate
-	dffe_ref dff0(q[0], d[0], clk, en, clr);
-	dffe_ref dff1(q[1], d[1], clk, en, clr);
-	dffe_ref dff2(q[2], d[2], clk, en, clr);
-	dffe_ref dff3(q[3], d[3], clk, en, clr);
-	dffe_ref dff4(q[4], d[4], clk, en, clr);
-	dffe_ref dff5(q[5], d[5], clk, en, clr);
-	dffe_ref dff6(q[6], d[6], clk, en, clr);
-	dffe_ref dff7(q[7], d[7], clk, en, clr);
-	dffe_ref dff8(q[8], d[8], clk, en, clr);
-	dffe_ref dff9(q[9], d[9], clk, en, clr);
-	dffe_ref dff10(q[10], d[10], clk, en, clr);
-	dffe_ref dff11(q[11], d[11], clk, en, clr);
-	dffe_ref dff12(q[12], d[12], clk, en, clr);
-	dffe_ref dff13(q[13], d[13], clk, en, clr);
-	dffe_ref dff14(q[14], d[14], clk, en, clr);
-	dffe_ref dff15(q[15], d[15], clk, en, clr);
-	dffe_ref dff16(q[16], d[16], clk, en, clr);
-	dffe_ref dff17(q[17], d[17], clk, en, clr);
-	dffe_ref dff18(q[18], d[18], clk, en, clr);
-	dffe_ref dff19(q[19], d[19], clk, en, clr);
-	dffe_ref dff20(q[20], d[20], clk, en, clr);
-	dffe_ref dff21(q[21], d[21], clk, en, clr);
-	dffe_ref dff22(q[22], d[22], clk, en, clr);
-	dffe_ref dff23(q[23], d[23], clk, en, clr);
-	dffe_ref dff24(q[24], d[24], clk, en, clr);
-	dffe_ref dff25(q[25], d[25], clk, en, clr);
-	dffe_ref dff26(q[26], d[26], clk, en, clr);
-	dffe_ref dff27(q[27], d[27], clk, en, clr);
-	dffe_ref dff28(q[28], d[28], clk, en, clr);
-	dffe_ref dff29(q[29], d[29], clk, en, clr);
-	dffe_ref dff30(q[30], d[30], clk, en, clr);
-	dffe_ref dff31(q[31], d[31], clk, en, clr);
-	
+	genvar i;
+	generate
+		for (i = 0; i < 32; i = i+1)
+		begin : dff_loop
+			dffe_ref dff0(q[i], d[i], clk, en, clr);
+		end
+	endgenerate
+//	dffe_ref dff0(q[0], d[0], clk, en, clr);
+//	dffe_ref dff1(q[1], d[1], clk, en, clr);
+//	dffe_ref dff2(q[2], d[2], clk, en, clr);
+//	dffe_ref dff3(q[3], d[3], clk, en, clr);
+//	dffe_ref dff4(q[4], d[4], clk, en, clr);
+//	dffe_ref dff5(q[5], d[5], clk, en, clr);
+//	dffe_ref dff6(q[6], d[6], clk, en, clr);
+//	dffe_ref dff7(q[7], d[7], clk, en, clr);
+//	dffe_ref dff8(q[8], d[8], clk, en, clr);
+//	dffe_ref dff9(q[9], d[9], clk, en, clr);
+//	dffe_ref dff10(q[10], d[10], clk, en, clr);
+//	dffe_ref dff11(q[11], d[11], clk, en, clr);
+//	dffe_ref dff12(q[12], d[12], clk, en, clr);
+//	dffe_ref dff13(q[13], d[13], clk, en, clr);
+//	dffe_ref dff14(q[14], d[14], clk, en, clr);
+//	dffe_ref dff15(q[15], d[15], clk, en, clr);
+//	dffe_ref dff16(q[16], d[16], clk, en, clr);
+//	dffe_ref dff17(q[17], d[17], clk, en, clr);
+//	dffe_ref dff18(q[18], d[18], clk, en, clr);
+//	dffe_ref dff19(q[19], d[19], clk, en, clr);
+//	dffe_ref dff20(q[20], d[20], clk, en, clr);
+//	dffe_ref dff21(q[21], d[21], clk, en, clr);
+//	dffe_ref dff22(q[22], d[22], clk, en, clr);
+//	dffe_ref dff23(q[23], d[23], clk, en, clr);
+//	dffe_ref dff24(q[24], d[24], clk, en, clr);
+//	dffe_ref dff25(q[25], d[25], clk, en, clr);
+//	dffe_ref dff26(q[26], d[26], clk, en, clr);
+//	dffe_ref dff27(q[27], d[27], clk, en, clr);
+//	dffe_ref dff28(q[28], d[28], clk, en, clr);
+//	dffe_ref dff29(q[29], d[29], clk, en, clr);
+//	dffe_ref dff30(q[30], d[30], clk, en, clr);
+//	dffe_ref dff31(q[31], d[31], clk, en, clr);
+//	
 	
 
 endmodule
